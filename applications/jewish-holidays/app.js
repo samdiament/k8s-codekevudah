246594ab9a6b863bd287df4aa007b88f6b619f42
Sam Diament <samdiament@outlook.com>
2024-03-12 20:48:18 -0400

    got form data to pass to controller

--- applications/jewish-holidays/app.js
+++ applications/jewish-holidays/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const formidable = require("formidable");
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -6,10 +9,30 @@ app.set('view engine', 'ejs');
 const PORT = 3000;
 app.use(express.json()) // -> req.body
 
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 // ROUTES
 app.get('/', (req, res) => {
     //res.status(200).send('hello world');
-    res.render('index');
+    res.render('pages/index', {
+        pageTitle: "Jewish Holidays"
+    });
+});
+
+app.get('/holidays', (req, res, next) => {
+
+    //const form = formidable({ multiples: true });  
+
+    console.log(req.query.holiday);
+
+    res.render('pages/holidays', {
+        pageTitle: "Jewish Holidays",
+        holiday: req.query.holiday,
+    });
 });
 
 
