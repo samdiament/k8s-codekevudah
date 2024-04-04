2924fd017abd834697c8eef2e663d01d1457ed36
Sam Diament <samdiament@outlook.com>
2024-04-04 11:38:02 -0400

    updated to call file read sync

--- applications/jewish-holidays/data/holidays.js
+++ applications/jewish-holidays/data/holidays.js
@@ -1,20 +1,12 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
-const express = require('express');
-const router = express.Router();
 
-const holidays = fs.readFile('./holidays.yml', 'utf8', function(e, data) {
-    var fileContents;
-    if (e) {
-        throw e;
-    }
-    fileContents = yaml.load(data, 'utf8');
-    //console.log(holidays);
-    return holidays;
-});
-
-module.exports = holidays;
-console.log(module.exports.holidays);
+var fileContents = fs.readFileSync('./holidays.yml', 'utf8');
+if (fileContents.length < 1) {
+    throw new Error ('Error: blank file');
+}
+holidays = yaml.load(fileContents, 'utf8');
+console.log(holidays);
 
 
 
